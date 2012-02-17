2f1b6ae841c4807958cc05cc577a78aa2bf01a74
konker <konker@gmail.com>
2012-02-17 16:36:39 +0200

    Initial commit of puppet bootstrap script

---
+++ puppet/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# assumes that this is run with the correct permissions
+
+# install the minimum needed for puppet to function
+sudo apt-get -y install git
+sudo apt-get -y install puppet
+
+# fetch the manifests.
+#git clone https://github.com/morningwoodsoftware/devops ~/devops
+
+# execute puppet on the manifest.
+#puppet apply ~/devops/puppet/init.pp
