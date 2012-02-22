afbe74a0f87c503b6b0a74d69f6fbb8a18f7a162
ubuntu <ubuntu@sputnik.morningwoodsoftware.com>
2012-02-22 15:57:22 +0000

    Removed redundant module

--- puppet/modules/system/manifests/init.pp
+++
@@ -1,6 +0,0 @@
-class system::init {
-    exec { "get_password":
-        command => "/home/ubuntu/devops/bin/get_password",
-        creates => "/home/ubuntu/devops/puppet/modules/system/manifests/secrets.pp",
-    }
-}
