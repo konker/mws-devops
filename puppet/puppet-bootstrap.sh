9d6e45a310cccbe209bd2be62fe38d63d5b21f3c
Konrad Markus <konker@gmail.com>
2012-05-09 19:03:27 +0300

    Added apt-get update to boostrap script

--- puppet/puppet-bootstrap.sh
+++ puppet/puppet-bootstrap.sh
@@ -2,11 +2,13 @@
 
 # assumes that this is run with the correct permissions
 
+sudo apt-get update
+
 # install the minimum needed for puppet to function
 sudo apt-get -y install git
 sudo apt-get -y install puppet
 
-mkdir ~/WORKING
+mkdir -f ~/WORKING
 
 # fetch the manifests.
 git clone https://github.com/morningwoodsoftware/devops ~/WORKING/devops
--- puppet/site.pp
+++ puppet/site.pp
@@ -3,6 +3,10 @@ $workstation_user = 'konker'
 
 $public_key = 'AAAAB3NzaC1yc2EAAAABIwAAAQEAu73i+JZ/KhqdAVYCkzQEGAzh14jpGa3m3V8y/At07psIWq1bQpAnADQQkqJNsm5xgloDDlOQBeQAI4NnK1E7/aD3wkF0tDGA1S8myGhAHxGvogv5X5Ujq1wnUfF+048lioRZxcHkzULGBX5uF2PIRXX/v1My7uxzx8sgv+aQnjqv67qCAM92mNubA4U72Yzoy4ZsBsUflvTwZm2RlBlXrwdRBxQUtce/QPHJoBCvVVLElp6E0HtHuYMRVbFBAdH/D1rNvJcrM97w7WXih+Q4jOQed9jmR+VSM1arqTPtNh8J2lOWHwrriONeK823WUVHS4cNZJMhKnKYCAC8HegN2Q=='
 
+$hostkeys = [
+    'github.com,207.97.227.239 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=='
+]
+
 node "base" {
     include sudo::sudo
     include ssh::sshd
