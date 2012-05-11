0624ff1e8b5f438edb950c8ff5deaca9ca5d87c4
Konrad Markus <konker@gmail.com>
2012-05-11 16:31:46 +0300

    Changed the way devops repo is cloned for sysadmin. The public key for sysadmin will need to be authorized by github.com for this to be functional

--- puppet/modules/user/manifests/devops.pp
+++ puppet/modules/user/manifests/devops.pp
@@ -4,7 +4,14 @@ define user::devops ($user) {
 
     # clone the devops repository from github
     exec { "$user/fetch-devops":
-        command => "/usr/bin/sudo -u $user /usr/bin/env HOME=/home/$user /usr/bin/git clone git@github.com:morningwoodsoftware/devops.git /home/$user/WORKING/devops",
+        command => "/usr/bin/sudo -u $user /usr/bin/env HOME=/home/$user /usr/bin/git clone https://konker@github.com/morningwoodsoftware/devops.git /home/$user/WORKING/devops",
+        creates => "/home/$user/WORKING/devops",
+        require => File["$user/WORKING"],
+    }
+
+    # Note: for this to be functional, syadmins public key will have to be authorized oin github.com
+    exec { "$user/add-devops-remote":
+        command => "/usr/bin/sudo -u $user /usr/bin/env HOME=/home/$user /usr/bin/git remote set-url origin git@github.com:morningwoodsoftware/devops.git",
         creates => "/home/$user/WORKING/devops",
         require => File["$user/WORKING"],
     }
