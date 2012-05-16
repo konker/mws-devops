ec1c1728c55d81fc5c779bade02a509471e21de6
Konrad Markus <konker@gmail.com>
2012-05-16 16:50:34 +0100

    Added support for post-update hook in the gitolite devops repo

--- puppet/modules/server/manifests/gitolite.pp
+++ puppet/modules/server/manifests/gitolite.pp
@@ -15,25 +15,35 @@ class server::gitolite {
     exec { "gl-setup":
         command => "/usr/bin/sudo -u git /usr/bin/env HOME=/home/git /usr/bin/gl-setup -q /var/keyshare/${::admin_user}_id_rsa.pub",
         creates => "/home/git/.gitolite",
-        require => [ Package['gitolite'], Exec["${::admin_user}/publish-key"] ]
+        require => [ Package['gitolite'], User::Publish_key["${::admin_user}"] ]
     }
 
-    ssh_authorized_key { "git/$::admin_user@morningwoodsoftware.com":
+    # authorize admin_user key for git user
+    ssh_authorized_key { "git:$::admin_user@morningwoodsoftware.com":
         # /dev/null specified so that puppet runs before actual public key file is generated
-        key => file("/home/$::admin_user/.ssh/id_rsa.pub", "/dev/null"),
+        key => file("/var/keyshare/${::admin_user}_id_rsa.pub", "/dev/null"),
         user => 'git',
         type => 'ssh-rsa',
         name => '',
         require => User[$::admin_user],
     }
 
-    ssh_authorized_key { "git/konker@morningwoodsoftware.com":
-        key => $::public_key,
-        user => 'git',
-        type => 'ssh-rsa',
-        name => 'konker@morningwoodsoftware.com',
+    # authorize 'master' key for git user
+    #ssh_authorized_key { "git:${::public_keys[0][0]}@morningwoodsoftware.com":
+    #    key => $::public_keys[0][1],
+    #    user => 'git',
+    #    type => 'ssh-rsa',
+    #    name => 'konker@morningwoodsoftware.com',
+    #}
+    
+    # mirror the devops repo under gitolite
+    exec { "mirror-devops":
+        command => "/usr/bin/sudo -u git /usr/bin/env HOME=/home/git /usr/bin/git clone --mirror $::devops_ro_git_url /home/git/repositories/devops.git",
+        creates => "/home/git/repositories/devops.git",
+        require => Exec['gl-setup'],
     }
 
+    # clone the gitolite-admin repo for admin_user
     exec { "$::admin_user/fetch-gitolite-admin":
         command => "/usr/bin/sudo -u $::admin_user /usr/bin/env HOME=/home/$::admin_user /usr/bin/git clone git@localhost:gitolite-admin.git /home/$::admin_user/WORKING/gitolite-admin",
         creates => "/home/$::admin_user/WORKING/gitolite-admin",
--- puppet/modules/user/manifests/user.pp
+++ puppet/modules/user/manifests/user.pp
@@ -1,4 +1,4 @@
-define user::user ($user, $groups, $authorize_public_key) {
+define user::user ($user, $groups) {
 
     user { "$user":
         ensure => present,
@@ -16,28 +16,14 @@ define user::user ($user, $groups, $authorize_public_key) {
         mode => 600,
     }
 
-    ssh_authorized_key { "${user}/master@morningwoodsoftware.com":
-        key => $authorize_public_key,
-        user => $user,
-        type => 'ssh-rsa',
-    }
-
     exec { "$user/ssh-keygen":
         command => "/usr/bin/sudo -u $user /usr/bin/env HOME=/home/$user /usr/bin/ssh-keygen -q -f /home/$user/.ssh/id_rsa -t rsa -N '' -C ${user}@morningwoodsoftware.com",
         creates => "/home/$user/.ssh/id_rsa.pub",
     }
 
-    exec { "$user/publish-key":
-        command => "/bin/cp /home/$user/.ssh/id_rsa.pub /var/keyshare/${user}_id_rsa.pub",
-        creates => "/var/keyshare/${user}_id_rsa.pub",
-        require => File['keyshare'],
-    }
-
     # ordering
     User[$user]
         -> File["$user/.ssh"] 
-        -> Ssh_authorized_key["${user}/master@morningwoodsoftware.com"] 
         -> Exec["$user/ssh-keygen"]
-        -> Exec["$user/publish-key"]
 }
 
