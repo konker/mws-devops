a50aa84115603ce121e1e065a98bf483924f1e91
konker <konker@gmail.com>
2012-05-18 10:04:22 +0100

    Re-org or devops post-update hook

--- puppet/modules/server/manifests/gitolite.pp
+++ puppet/modules/server/manifests/gitolite.pp
@@ -43,6 +43,17 @@ class server::gitolite {
         require => Exec['gl-setup'],
     }
 
+    # create git post-update hook for the devops repo under gitolte
+    file { "devops-post-update":
+        path    => '/home/git/repositories/devops.git/hooks/post-update.secondary',
+        ensure  => file,
+        source  => 'puppet:///modules/server/devops/post-update',
+        owner   => 'git',
+        group   => 'git',
+        mode    => 755,
+        require => Exec['mirror-devops'],
+    }
+
     # clone the gitolite-admin repo for admin_user
     exec { "$::admin_user/fetch-gitolite-admin":
         command => "/usr/bin/sudo -u $::admin_user /usr/bin/env HOME=/home/$::admin_user /usr/bin/git clone git@localhost:gitolite-admin.git /home/$::admin_user/WORKING/gitolite-admin",
