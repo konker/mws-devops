10527454ce886a7a3b66e84d6b031ee8aabc11c5
Konrad Markus <konker@gmail.com>
2012-05-29 17:56:08 +0300

    Trying find inside generate to bypass parse-time checks

--- puppet/modules/user/manifests/publish_key.pp
+++ puppet/modules/user/manifests/publish_key.pp
@@ -4,7 +4,8 @@ define user::publish_key ($user, $key='') {
         file { "$user/publish_key":
             path    => "/var/keyshare/${user}_id_rsa.pub",
             ensure  => file,
-            content => generate('/usr/bin/cut', '-c', '9-', "/home/$user/.ssh/id_rsa.pub"),
+            content => generate('/usr/bin/find', "/home/$user/.ssh", '-name', 'id_rsa.pub', '-exec', '/usr/bin/cut', '-c', '9-', '{}', '\\;'),
+            #content => generate('/usr/bin/cut', '-c', '9-', "/home/$user/.ssh/id_rsa.pub"),
             #content => file("/home/$user/.ssh/id_rsa.pub", '/dev/null'),
             require => File['keyshare'],
         }
